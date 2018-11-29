44eac548ad6503c7efac4df5a127c1c8adec7beb
Kayla Perry <kperry42@uncc.edu>
2018-11-29 19:37:02 +0000

    Add validations to New Contact Form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,6 +12,7 @@ class ContactsController < ApplicationController
     
     
     def new
+      @contact = Contact.new
     end
     
     def create
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
 class Contact < ApplicationRecord
     has_many :events, dependent: :destroy
+    validates :last, presence: true, length: {minimum: 2}
+    validates :first, presence: true, length: {minimum: 2}
+    validates :address,presence: true, length: {minimum: 8}
+    validates :home,  numericality: { only_integer: true }, length: { is: 10 }, allow_blank: true
+    validates :cell,  numericality: { only_integer: true }, length: { is: 10 }, allow_blank: true
+    validates :home, numericality: { only_integer: true }, length: { is: 10 }, allow_blank: true
 end
